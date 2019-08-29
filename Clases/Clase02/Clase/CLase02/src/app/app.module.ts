0f0468a2239cfe8b61326701937cb1c63a715636
Julián N Graziano <Julian.N.Graziano@gds.ey.com>
2019-08-29 19:33:11 -0300

    Clase02

---
+++ Clases/Clase02/Clase/CLase02/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListadoDeUsuariosComponent } from './listado-de-usuarios/listado-de-usuarios.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoDeUsuariosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Clases/Clase02/Clase/CLase02/src/app/listado-de-usuarios/listado-de-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-de-usuarios',
+  templateUrl: './listado-de-usuarios.component.html',
+  styleUrls: ['./listado-de-usuarios.component.scss']
+})
+export class ListadoDeUsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
